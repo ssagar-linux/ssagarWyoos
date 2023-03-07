46046a4da286a57961bd6201b1644dfea71892c9
Sourabh Sagar <sourabhsagar7601@gmail.com>
2023-03-08 00:59:43 +0530

    [PIC] In series of writing your own OS in cpp 3) Added Support to communicate with Hardware ports via PIC(Programmable Interrupt Controller)    through which interrupts from Hardware are passed to CPU
    ++ Modified printf to write in 80 X 25 screen size instead of writing from same location(0xb8000) again and again
    ++ clean functionality added in Makefilew

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ GPPPARAMS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-excep
 ASPARAMS = --32 
 LDPARAMS = -melf_i386
 
-objects = loader.o gdt.o kernel.o
+objects = loader.o gdt.o port.o kernel.o
 
 %.o: %.cpp
 	gcc $(GPPPARAMS) -o $@ -c $<
@@ -33,3 +33,8 @@ mykernel.iso: mykernel.bin
 run: mykernel.iso
 	(killall VirtualBoxVM && sleep 1) || true
 	VirtualBoxVM --startvm  "My Operating System" &
+
+
+.PHONY: clean
+clean:
+	rm -f $(objects) mykernel.bin mykernel.iso
--- kernel.cpp
+++ kernel.cpp
@@ -5,9 +5,38 @@
 void printf(char* str)
 {
 	static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+	static uint8_t x=0, y=0;
 
 	for(int i=0; str[i] != '\0'; ++i)
-		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+	{
+		switch(str[i])
+		{
+			case '\n':
+				y++;
+				x = 0;
+				break;
+			default:
+				VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+				x++;
+				break;
+		}
+
+		if(x >= 80)
+		{
+			y++;
+			x = 0;
+		}
+
+		if(y >= 25)
+		{
+			// clear the screen and start writing from x=0,y=0
+			for(y = 0; y < 25; y++)
+				for(x = 0; x < 80; x++)
+					VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+
+			x = 0; y =0;
+		}
+	}
 }
 
 /*
@@ -23,6 +52,7 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(void* multiboot_structure, uint32_t multiboot_magicnum)
 {
+	printf("Hello World! --- http://www.ssagarLinux.in\n");
 	printf("Hello World! --- http://www.ssagarLinux.in");
 
 	GlobalDescriptorTable gdt;
